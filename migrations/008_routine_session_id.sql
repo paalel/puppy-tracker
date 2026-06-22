0e9f6c10d58ee4c1ea758e0b71c89665cc75bae4
Pål Erik Lystad <pal.lystad@gmail.com>
2026-06-22 23:49:04 +0200

    Add reusable css components

--- migrations/008_routine_session_id.sql
+++ migrations/008_routine_session_id.sql
@@ -1,14 +1,13 @@
 ALTER TABLE sessions ADD COLUMN routine_session_id INTEGER REFERENCES routine_sessions(id);
 
-UPDATE sessions
-SET routine_session_id = (
-    SELECT rs.id
-    FROM routine_sessions rs
-    ORDER BY rs.position
-    LIMIT 1 OFFSET (
-        SELECT COUNT(*) - 1
-        FROM sessions s2
-        WHERE s2.date = sessions.date AND s2.id <= sessions.id
-    )
-)
-WHERE routine_session_id IS NULL;
+CREATE TEMPORARY TABLE _pos AS
+SELECT s.id AS sid,
+       (SELECT COUNT(*) FROM sessions prev WHERE prev.date = s.date AND prev.id < s.id) AS idx
+FROM sessions s;
+
+UPDATE sessions SET routine_session_id = (
+    SELECT rs.id FROM routine_sessions rs
+    INNER JOIN _pos ON _pos.sid = sessions.id AND rs.position = _pos.idx + 1
+) WHERE routine_session_id IS NULL;
+
+DROP TABLE _pos;
